abb3cc511b1daa82442f93fb995bbfbc80d23a74
tirrexx-x64 <tirtarendysi77@gmail.com>
2026-03-03 20:49:33 +0700

    ok

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -0,0 +1,60 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    private static final String REDIRECT_CAR_LIST = "redirect:/car/list";
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/create")
+    public String createCarPage(Model model) {
+        model.addAttribute("car", new Car());
+        return "createCar";
+    }
+
+    @PostMapping("/create")
+    public String createCarPost(@ModelAttribute Car car) {
+        carService.create(car);
+        return REDIRECT_CAR_LIST;
+    }
+
+    @GetMapping("/list")
+    public String carListPage(Model model) {
+        List<Car> cars = carService.findAll();
+        model.addAttribute("cars", cars);
+        return "carList";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCarPage(@PathVariable String id, Model model) {
+        Car car = carService.findById(id);
+        if (car == null) return REDIRECT_CAR_LIST;
+        model.addAttribute("car", car);
+        return "editCar";
+    }
+
+
+    @PostMapping("/update")
+    public String updateCar(@ModelAttribute Car car) {
+        carService.update(car.getCarId(), car);
+        return REDIRECT_CAR_LIST;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCar(@PathVariable String id) {
+        carService.deleteCarById(id);
+        return REDIRECT_CAR_LIST;
+    }
+}
