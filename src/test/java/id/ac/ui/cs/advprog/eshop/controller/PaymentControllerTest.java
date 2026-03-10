14d25b71eb8a3a0d3ba5508374807dd100efa198
tirrexx-x64 <tirtarendysi77@gmail.com>
2026-03-10 10:14:59 +0700

    [RED] Add functional tests for PaymentController

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PaymentController.class)
+class PaymentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    @Test
+    void testAdminListPage() throws Exception {
+
+        mockMvc.perform(get("/payment/admin/list"))
+                .andExpect(status().isOk());
+    }
+
+}
