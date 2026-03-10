e5070611f310126babd1640f5f944933618f900d
tirrexx-x64 <tirtarendysi77@gmail.com>
2026-03-10 10:13:59 +0700

    [RED] Add functional tests for OrderController

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    @Test
+    void testCreatePage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testHistoryPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk());
+    }
+}
